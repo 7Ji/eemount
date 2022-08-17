cb4f7ea9195798aa33b8962cfc88558e36ef657d
7Ji <pugokushin@gmail.com>
2022-08-17 10:26:34 +0800

    util: add doc

--- include/util.h
+++ include/util.h
@@ -2,7 +2,20 @@
 #define HAVE_UTIL_H
 #include "common.h"
 
+/**
+ * @brief Convert an unsigned long to unsigned integer safely, limit the range between 0~UINT_MAX
+ * 
+ * @param value The unsinged long to convert
+ * @return unsigned int The converted unsinged integer
+ */
 unsigned int util_uint_from_ulong(unsigned long value);
+
+/**
+ * @brief Convert a long to integer safely, limit the range between INT_MIN~INT_MAX
+ * 
+ * @param value The long to convert
+ * @return int The converted integer
+ */
 int util_int_from_long(long value);
 
 #endif
